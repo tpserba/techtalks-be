ec938864ed08b23333b204a7b5cf5d2304da8a64
Tomás Praga <tpraga@serbatic.es>
2023-04-20 09:47:55 +0200

    implement SSL

--- src/main/java/com/serbatic/tpraga/techtalks/TechtalksApplication.java
+++ src/main/java/com/serbatic/tpraga/techtalks/TechtalksApplication.java
@@ -1,7 +1,14 @@
 package com.serbatic.tpraga.techtalks;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TechtalksApplication {
@@ -10,4 +17,38 @@ public class TechtalksApplication {
 		SpringApplication.run(TechtalksApplication.class, args);
 	}
 
+
+	@Bean
+	public ServletWebServerFactory servletContainer(){
+		// Enable SSL Traffic
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(){
+			@Override
+			protected void postProcessContext(Context context){
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		// Add HTTP to HTTPS redirect
+		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+		return tomcat;
+	}
+
+	/**
+	 * Redirects from HTTP to HTTPS
+	 * Without SSL the app used port 8080. With SSl it will use 8443.
+	 * So requests to 8080 must be redirected to 8443
+	 * @return
+	 */
+	private Connector httpToHttpsRedirectConnector() {
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
+	}
 }
